0cdf3c5caec5d603d044435b0a906eced04e113a
Jayashree-Vasamsetti <vasamsettijayashree@gmail.com>
2026-08-21 18:50:41 +0530

    threads

--- src/main/java/com/Playground/Singleton.java
+++ src/main/java/com/Playground/Singleton.java
@@ -8,14 +8,35 @@ public class Singleton {
         // private constructor to prevent instantiation
     }
 
-    public static Singleton getInstance() {
-        if (instance == null) {
-            instance = new Singleton();
-        }
-        return instance;
-    }
+//    public static Singleton getInstance() {
+//        if (instance == null) {
+//            instance = new Singleton();
+//        }
+//        return instance;
+//    }
+//
+//    public void showMessage() {
+//        System.out.println("Hello from Singleton!");
+//    }
 
-    public void showMessage() {
-        System.out.println("Hello from Singleton!");
-    }
+    //to make it accessible in multithreading environment
+//Part 2: Multithreading environment
+//    public static  synchronized  Singleton getInstance(){
+//        if (instance == null) {
+//            instance= new Singleton();
+//        }
+//        return instance;
+//        }
+// part 3 instead of sunchronising the complete method we can synchronise the block of code
+        public static  Singleton getInstance() {
+            if (instance == null) {
+                synchronized (Singleton.class) {
+                    if (instance == null) {
+                        instance = new Singleton();
+                    }
+                }
+            }
+            return instance;
+
+        }
 }
